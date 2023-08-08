b33a6b7b08847fc03352bd4eff7ddde328752bd3
Mahi samia <mahisamia13@gmail.com>
2023-08-08 11:39:47 +0100

    uploading base code

---
+++ app.js
@@ -0,0 +1,18 @@
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+
+document.addEventListener("DOMContentLoaded", function () {
+  let clearBtn = document.querySelector(".clear");
+  let equalBtn = document.querySelector(".equal");
+  let decimalSymbol = document.querySelector(".decimal");
+  let plusMinusBtn = document.querySelector(".plus-minus");
+  let deleteBtn = document.querySelector(".delete");
+
+  let numbersList = document.querySelectorAll(".number");
+
+  let operatorsList = document.querySelectorAll(".operator");
+  let previousDisplay = document.querySelector(".previous");
+  let currentDisplay = document.querySelector(".current");
+  currentDisplay.textContent = currentValue;
+});
